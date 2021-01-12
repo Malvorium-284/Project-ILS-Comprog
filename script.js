c8e672c7ebf0cea4ec97ef6e78a35ca7d9414f20
Malvorium-284 <71530780+Malvorium-284@users.noreply.github.com>
2021-01-12 23:42:10 +0800

    add important files & updates
    added images for website
    html, js, and css have been updated.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 
 window.onload = function() {
     if (window.jQuery) {
-        alert("SysCheck: jQuery is working!");
+        console.log("Jquery is working properly.");
     } else {
         alert("SysCheck Alert: jQuery not detected/not working.");
     }
+
+    var global_Balance;
+
+    const components = { // add data about different components here
+        processor: {    
+            
+        },
+
+        graphicsCard: {
+
+        },
+
+        RAM: {
+
+        },
+
+        StorageDrive: {
+
+        }
+    }
+
+    $(".grid_Item").addClass("ContentHidden");
 
     $("#enterBTN").click(function() {
         console.log("Button was clicked.");
@@ -18,14 +40,26 @@ window.onload = function() {
         if (isNaN(inputValue)) {
             $("#errorMsg").text("Please type a whole number");
             $(".grid_Money").css({"border": "4px red solid"});
+        } else if (inputValue < 10000) {
+            $("#errorMsg").text("Value has to be > 10000.");
+            $(".grid_Money").css({"border": "4px red solid"});
         } else {
-            $("#errorMsg").text("Operation is successful.");
-            $(".grid_Money").css({"border": "2px blue solid"});
+            $("#errorMsg").text("Operation successful.");
+            inputValid();
+            global_Balance = inputValue;
+            console.log(`Balance: ${inputValue}, global variable`);
+            $("#budgetContent").html(`P ${inputValue} <br> - Current Budget - <br> Items available to you is based on current budget`);
         }
     });
 
-        
-
-
+    function inputValid() {
+        $(".grid_Item").removeClass("ContentHidden").addClass("ContentVisible");
+        /*
+        I used an alternative to showing elements as opposed to show()
+        since I wanted an animation on the grid container
+        */
+        $(".grid_Money").css({"border": "2px blue solid"});
+    }
+  
 
 }
