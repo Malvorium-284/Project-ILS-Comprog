80d4580a89333c500e9ac581ef22ae4da67cb2e0
Malvorium-284 <71530780+Malvorium-284@users.noreply.github.com>
2021-01-13 07:51:48 +0800

    important additions
    additions & updates

--- script.js
+++ script.js
@@ -6,36 +6,202 @@ window.onload = function() {
         alert("SysCheck Alert: jQuery not detected/not working.");
     }
 
-    var global_Balance;
+    var global_isOptionsAdded; 
+    var global_isOptionsChangedOnce;
+    var total_processorNames = 0;
+    var total_graphicCardNames = 0;
+    var total_ramNames = 0;
+    var total_storageNames = 0;
 
-    const components = { // add data about different components here
+    const components = { // add data about PC components here. follow pattern of setting properties.
+
         processor: {    
-            
+            Pentium_G5400: {
+                name: 'Intel Pentium G5400 3.7 GHz | 2 Cores | 4 Threads | P2800',
+                brand: 'intel',
+                price: 2800,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=29&product_id=689&sort=p.price&order=ASC',
+                value: 1
+            },
+
+            i3_9100F: {
+                name: 'Intel Core i3-9100F 3.60 GHz | 4 Cores | 4 Threads | P3930',
+                brand: 'intel',
+                price: 3930,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=29&product_id=690&sort=p.price&order=ASC',
+                value: 2
+            },
+
+            Ryzen_3_2200: {
+                name: 'AMD Ryzen 3 2200G 3.5 GHz | 4 Cores | P5100',
+                brand: 'amd',
+                price: 5100,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=29&product_id=67&sort=p.price&order=ASC',
+                value: 3
+            },
+
+            Ryzen_3_3100: {
+                name: 'AMD Ryzen 3 3100 3.6 GHz | 4 Cores | 8 Threads | P5750',
+                brand: 'amd',
+                price: 5750,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=29&product_id=677&sort=p.price&order=ASC',
+                value: 4
+            },
+
+            Ryzen_3_3200: { 
+                name: 'AMD Ryzen™ 3 3200G with Radeon™ Vega 8 Graphics | 4 Cores | 8 Threads | P5950',
+                brand: 'amd',
+                price: 5950,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=29&product_id=678&sort=p.price&order=ASC',
+                value: 5
+            }   
         },
 
         graphicsCard: {
+            MSI_GT_1030: {
+                name: 'MSI GT 1030 | 2GB DDR4 | Overclocked | P3900',
+                brand: 'msi',
+                inventor: 'nvidia',
+                price: 3900,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=156&sort=p.price&order=ASC',
+                value: 1
+            },
 
+            GTX_1050Ti: {
+                name: 'MSI GTX 1050Ti | 4GB GDDR5 | Overclocked | P7300',
+                brand: 'msi', 
+                inventor: 'nvidia',
+                price: 7300,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=602&sort=p.price&order=ASC',
+                value: 2
+            },
+
+            RX_560: {
+                name: 'MSI RX 560 | 4GB GDDR5 | Overclocked | P8100',
+                brand: 'msi',
+                inventor: 'amd',
+                price: 8100,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=191&sort=p.price&order=ASC',
+                value: 3
+            },
+
+            ASUS_GTX_1050Ti: {
+                name: 'ASUS PHOENIX GTX 1050Ti | 4GB GDDR5 | Non-Overclocked | P8990',
+                brand: 'asus',
+                inventor: 'amd',
+                price: 8990,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=196&sort=p.price&order=ASC',
+                value: 4
+            },
+
+            GTX_1050: {
+                name: 'ASUS GTX 1050 | 2GB GDDR5 | Non-Overclocked | P7900',
+                brand: 'asus',
+                inventor: 'nvidia',
+                price: 7900,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=195&sort=p.price&order=ASC',
+                value: 5
+            },
+
+            ZOTAC_GTX_1050: {   
+                name: 'ZOTAC GTX 1050 | 2GB GDDR5 | Overclocked | P8300',
+                brand: 'zotac',
+                inventor: 'nvidia',
+                price: 8300,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=65&product_id=235&sort=p.price&order=ASC',
+                value: 6
+            }
         },
 
         RAM: {
+            K_4GB_DDR4: {
+                name: 'Kingston 4GB | DDR4 | 2666MHz | P1140',
+                brand: 'Kingston',
+                price: 1140,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=64&product_id=705&sort=p.price&order=ASC',
+                value: 1
+            },
 
+            K_4GB_DDR3: {
+                name: 'Kingston 4GB | DDR3 | 1600 MHz | P1150',
+                brand: 'Kingston',
+                price: 1150,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=64&product_id=345&sort=p.price&order=ASC',
+                value: 2
+            },
+
+            K_8GB_DDR4: {
+                name: 'Kingston 8GB | DDR4 | 2666 MHz | P1740',
+                brand: 'Kingston',
+                price: 1740,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=64&product_id=706&sort=p.price&order=ASC',
+                value: 3
+            },
+
+            K_16GB_DDR4: {
+                name: 'Kingston 16GB | DDR4 | 2666 MHz | P3100',
+                brand: 'Kingston',
+                price : 3100,
+                link: 'http://pcgilmore.com.ph/index.php?route=product/product&path=64&product_id=710&sort=p.price&order=ASC',
+                value: 4
+            }   
         },
 
         StorageDrive: {
+                Seagate_500GB: {
+                    name: 'Seagate 500GB | 7200 RPM | HDD | P2150',
+                    brand: 'Seagate',
+                    price: 2150,
+                    link: 'http://pcgilmore.com.ph/index.php?route=product/product&product_id=100&tag=hard+disk+drive',
+                    value: 1
+                },
+
+                WD_500GB: {
+                    name: 'Western Digital Blue 500GB | 7200 RPM | HDD | P2350',
+                    brand: 'Western Digital',
+                    price: 2350,
+                    link: 'http://pcgilmore.com.ph/index.php?route=product/product&product_id=108&tag=hard+disk+drive&sort=p.price&order=ASC',
+                    value: 2
+                },
+
+                WD_1TB: {
+                    name: 'Western Digital Blue 1TB | 7200 RPM | HDD | P2600',
+                    brand: 'Western Digital',
+                    price: 2600,
+                    link: 'http://pcgilmore.com.ph/index.php?route=product/product&product_id=109&tag=hard+disk+drive&sort=p.price&order=ASC',
+                    value: 3
+                },
+
+                WD_2TB: {
+                    name: 'Western Digital Blue 2TB | 7200 RPM | HDD | P3850',
+                    brand: 'Western Digital',
+                    price: 3850,
+                    link: 'http://pcgilmore.com.ph/index.php?route=product/product&product_id=110&tag=hard+disk+drive&sort=p.price&order=ASC',
+                    value: 4 
+                },
+
+                WD_4TB: {
+                    name: "Western Digital Blue 4TB | 7200 RPM | HDD | P6750",
+                    brand: 'Western Digital',
+                    price: 6750,
+                    link: 'http://pcgilmore.com.ph/index.php?route=product/product&product_id=111&tag=hard+disk+drive&sort=p.price&order=ASC',
+                    value: 5
+                }
 
         }
     }
 
     $(".grid_Item").addClass("ContentHidden");
+    
 
     $("#enterBTN").click(function() {
         console.log("Button was clicked.");
         console.log($(".moneyInput").text());
         // debug; check if it prints to the console for verification
 
-        let inputValue = parseInt($(".moneyInput").text());
+        let inputValue = parseInt($(".moneyInput").text(), 10);
         console.log(inputValue);
-        console.log(typeof inputValue);
+        console.log("Input Data Type: " + typeof inputValue);
 
         if (isNaN(inputValue)) {
             $("#errorMsg").text("Please type a whole number");
@@ -46,9 +212,63 @@ window.onload = function() {
         } else {
             $("#errorMsg").text("Operation successful.");
             inputValid();
-            global_Balance = inputValue;
             console.log(`Balance: ${inputValue}, global variable`);
-            $("#budgetContent").html(`P ${inputValue} <br> - Current Budget - <br> Items available to you is based on current budget`);
+            $("#number_Money").text(`${inputValue}`);
+            $("#budgetContent").html(`- Current Budget - <br> If you see Red budget, it means that your current choices combined exceeds your current budget. Please re-evaluate your choices.`);
+            loadOptionItems();
+        }
+    });
+
+    $("#debugBTN").click(function() {
+        testAddOption()
+    });
+
+    $("#Processor").change(function() {
+        for (let optAttrValue = 0; optAttrValue <= total_processorNames; optAttrValue++) {
+            if ($(this).val() == optAttrValue) {
+                for (let model in components.processor) {
+                    if (`${components.processor[model].value}` == optAttrValue) {
+                        $("#processorPrice").text(`Price: ${components.processor[model].price}`);
+                    }
+                }
+            }
+        } 
+    });
+
+    $("#Graphics_Card").change(function() {
+        for (let optAttrValue = 0; optAttrValue <= total_graphicCardNames; optAttrValue++) {
+            if ($(this).val() == optAttrValue) {
+                for (let model in components.graphicsCard) {
+                    if (`${components.graphicsCard[model].value}` == optAttrValue) {
+                        $("#graphicsPrice").text(`Price: ${components.graphicsCard[model].price}`);
+                    }
+                }
+            }
+        }
+    });
+
+  
+
+    $("#RAM_Memory").change(function() {
+        for (let optAttrValue = 0; optAttrValue <= total_ramNames; optAttrValue++) {
+            if ($(this).val() == optAttrValue) {
+                for (let model in components.RAM) {
+                    if (`${components.RAM[model].value}` == optAttrValue) {
+                        $("#ramPrice").text(`Price: ${components.RAM[model].price}`);
+                    }
+                }
+            }
+        }
+    });
+    $("#Storage_Drive").change(function() {
+        for (let optAttrValue = 0; optAttrValue <= total_storageNames; optAttrValue++) {
+            if ($(this).val() == optAttrValue) {
+                for (let model in components.StorageDrive) {
+                    if (`${components.StorageDrive[model].value}` == optAttrValue) {
+                        $("#storagePrice").text(`Price: ${components.StorageDrive[model].price}`);
+                    }
+                }
+            }
         }
     });
 
@@ -59,6 +279,52 @@ window.onload = function() {
         since I wanted an animation on the grid container
         */
         $(".grid_Money").css({"border": "2px blue solid"});
+    }
+
+    function loadOptionItems() {
+        if (!global_isOptionsAdded) { // check if options were already added. avoids duplication
+            global_isOptionsAdded = true;
+            let procAttrVal = 0;
+            let graphicAttrVal = 0;
+            let ramAttrVal = 0;
+            let storageAttrVal = 0;
+
+               // processor, add options to drop-down from components object
+            for (let model in components.processor) {
+                ++procAttrVal;
+                ++total_processorNames;
+                // console.log(`${model}: Price is ${components.processor[model].price}`);
+                $("select#Processor").append(`<option value="${procAttrVal}" id="${procAttrVal}"> ${components.processor[model].name} </option>`);
+            }
+
+                // graphics card, add options to drop-down from components object
+            for (let model in components.graphicsCard) {
+                ++total_graphicCardNames
+                ++graphicAttrVal;
+                $("select#Graphics_Card").append(`<option value="${graphicAttrVal}" id="${graphicAttrVal}"> ${components.graphicsCard[model].name} </option>`);
+            }
+
+                // RAM, add options to drop-down from components object
+            for (let model in components.RAM) {
+                ++total_ramNames
+                ++ramAttrVal;
+                $("select#RAM_Memory").append(`<option value="${ramAttrVal}" id="${ramAttrVal}"> ${components.RAM[model].name} </option>`);
+            }
+
+                // Storage, add options to drop-down from components object
+            for (let model in components.StorageDrive) {
+                ++total_storageNames;
+                ++storageAttrVal;
+                $("select#Storage_Drive").append(`<option value="${storageAttrVal}" id="${storageAttrVal}"> ${components.StorageDrive[model].name} </option>`);
+            }
+        }
+    }
+
+    
+
+    function testAddOption() {
+        console.log("called function debug");
+        $("select#Processor").append(`<option value="test"> Test </option>`);
     }
   
 
